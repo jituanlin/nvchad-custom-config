b681a6313c6c8582735ea149b6015ee65ce9789b
jituanlin <jituanlin@tencent.com>
2023-03-23 21:28:21 +0800

    turn on nvim_tree when vim_enter

--- init.lua
+++ init.lua
@@ -1,7 +1,16 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+local function open_nvim_tree()
+
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+autocmd({"VimEnter"}, { pattern ="*", callback = open_nvim_tree })
+
