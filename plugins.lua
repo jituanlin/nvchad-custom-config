1c59f7fa63479873d330d0baba71242624522771
jituanlin <jituanlin@tencent.com>
2023-04-12 16:40:08 +0800

    add nvim-surround and set theme

--- plugins.lua
+++ plugins.lua
@@ -5,6 +5,17 @@ local plugins = {
 
   -- Override plugin definition options
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
+  {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
